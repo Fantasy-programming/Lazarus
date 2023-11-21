1c5c190fc8fd2f0047142e79af1a46230f00b17a
gamernewone <37631123+gamernewone@users.noreply.github.com>
2023-11-21 15:25:03 +0000

    Add SVGR to the project

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "@svgr/rollup";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -7,8 +8,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
       "@pages": path.resolve(__dirname, "./src/pages"),
+      "@modules": path.resolve(__dirname, "./src/modules"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
-  plugins: [react()],
+  plugins: [react(), svgr()],
 });

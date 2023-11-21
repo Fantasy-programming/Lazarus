ce56f1583f400c039e960a8e6842868a6592b2c2
gamernewone <37631123+gamernewone@users.noreply.github.com>
2023-11-21 19:36:48 +0000

    Add autoanimate + simple state logic

--- src/components/molecules/ChatContainer/index.jsx
+++ src/components/molecules/ChatContainer/index.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+
 import ChatInput from "@components/atoms/ChatInput";
 import ChatBtn from "@components/atoms/ChatBtn";
 import ChatBubble from "@components/atoms/ChatBubble";
@@ -6,6 +9,18 @@ import Style from "./index.module.css";
 import Spacing from "@modules/utility.module.css";
 
 function ChatContainer() {
+  const [chat, setChat] = useState([]);
+  const [message, setMessage] = useState("");
+  const [parent] = useAutoAnimate({
+    duration: 150,
+    easing: "ease-in-out",
+  });
+  const sendMessage = (e) => {
+    e.preventDefault();
+    setChat([...chat, message]);
+    setMessage("");
+  };
+
   return (
     <div className={Style.container}>
       <div className={Style.privacy}>
@@ -15,14 +30,21 @@ function ChatContainer() {
         </div>
         <div> + </div>
       </div>
-      <div className={Style.chat}>
+      <div className={Style.chat} ref={parent}>
         <ChatBubble person="left" />
         <ChatBubble person="right" />
         <ChatBubble person="left" />
+        {chat.map((message, index) => (
+          <ChatBubble key={index} person="left" message={message} />
+        ))}
       </div>
-      <form className={Style.chatbar}>
+      <form className={Style.chatbar} onSubmit={sendMessage}>
         <ChatBtn icon="paperclip" color="secondary" />
-        <ChatInput className={Spacing["m-x-1"]} />
+        <ChatInput
+          className={Spacing["m-x-1"]}
+          value={message}
+          action={setMessage}
+        />
         <ChatBtn icon="plane" color="red-gradient" />
       </form>
     </div>

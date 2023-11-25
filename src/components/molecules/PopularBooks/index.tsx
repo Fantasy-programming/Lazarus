572f8a71075e8d0c0ed8e4d0cad1c02f274e0b20
Fantasy <37631123+Fantasy-programming@users.noreply.github.com>
2023-11-25 17:45:20 +0000

    Add types & move data to the db

--- src/components/molecules/PopularBooks/index.tsx
+++ src/components/molecules/PopularBooks/index.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
+import bookService from "@/services/bookService";
+
 import Book from "@/components/atoms/Book";
 import Style from "./index.module.css";
 import LeftIcon from "@/assets/left.svg?react";
 import RightIcon from "@/assets/right.svg?react";
 
 function PopularBooks() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    bookService.getAll().then((response) => {
+      setBooks(response);
+    });
+  }, []);
+
   return (
     <div>
       <div className={Style.header}>
@@ -20,42 +31,9 @@ function PopularBooks() {
       </div>
 
       <div className={Style.container}>
-        <Book
-          title="Fairy Tale"
-          author="Stephen King"
-          color="hsl(210, 97%, 25%)"
-          cover="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1647789287i/60177373.jpg"
-        />
-        <Book
-          title="Never After"
-          author="Stephani gueber"
-          color="hsl(253, 33%, 36%)"
-          cover="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1645642179i/59808071.jpg"
-        />
-        <Book
-          title="Klara and the Sun"
-          author="Kazuo Ishiguro"
-          color="hsl(2, 100%, 65%)"
-          cover="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1603206535i/54120408.jpg"
-        />
-        <Book
-          title="Mist and Fury"
-          author="Sarah J. Maas"
-          color="hsl(173, 99%, 34%)"
-          cover="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1620325671i/50659468.jpg"
-        />
-        <Book
-          title="hamnet"
-          author="Maggie O'Farrell            "
-          color="hsl(213, 74%, 27%)"
-          cover="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1574943819i/43890641.jpg"
-        />
-        <Book
-          title="wellness"
-          author="nathan hill"
-          color="hsl(179, 100%, 34%)"
-          cover="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1695742140i/65650229.jpg"
-        />
+        {books.map((book) => {
+          return <Book key={book.id} title={book.title} author={book.author} color={book.color} cover={book.cover} />;
+        })}
       </div>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@components": path.resolve(__dirname, "./src/components"),
+      "@services": path.resolve(__dirname, "./src/services"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@modules": path.resolve(__dirname, "./src/modules"),
